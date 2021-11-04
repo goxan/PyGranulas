27daf8e1375c39e5a869dbf915bd25c67356fc0b
goxan <30704972+goxan@users.noreply.github.com>
2021-11-04 16:42:09 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cov-spec',
+      version='1.0',
+      description='Python utility to calculate and vizualize coverage and specificity',
+      author='Yaroslav Plaksin',
+      author_email='y.plaksin@innopolis.university',
+      packages=['cov_spec'],
+     )
