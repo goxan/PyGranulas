ead59fb204dd7ce7e8fabceda3ae7c1500c49617
goxan <30704972+goxan@users.noreply.github.com>
2021-12-07 18:37:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='coverage_specificity',
-      version='1.0.2',
+      version='1.0.3',
       description='Python utility to calculate and vizualize coverage and specificity',
       author='Yaroslav Plaksin',
       url='https://github.com/goxan/coverage-specificity',
