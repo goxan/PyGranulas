14e18749d9474803cec846c92686cb7394f0b675
goxan <30704972+goxan@users.noreply.github.com>
2021-12-07 18:34:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='coverage_specificity',
-      version='1.0.1',
+      version='1.0.2',
       description='Python utility to calculate and vizualize coverage and specificity',
       author='Yaroslav Plaksin',
       url='https://github.com/goxan/coverage-specificity',
       author_email='y.plaksin@innopolis.university',
-      packages=['cov_spec'],
-      install_requires=['validclust', 'tqdm']
+      packages=['coverage_specificity'],
+      install_requires=['validclust']
      )
